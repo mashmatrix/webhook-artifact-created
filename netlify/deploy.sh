18531a486fbb1334aaef8c474461d204c05d3e31
Shinichi Tomita <shinichi.tomita@gmail.com>
2023-01-14 20:59:29 +0900

    rever to original

--- netlify/deploy.sh
+++ netlify/deploy.sh
@@ -10,7 +10,6 @@ fi
 DOWNLOAD_URL=$(echo "${INCOMING_HOOK_BODY}" | jq -r .download_url | base64 -d)
 ARTIFACT_ID=$(echo "${INCOMING_HOOK_BODY}" | jq -r .artifact_id)
 
-pushd /tmp
-wget "${DOWNLOAD_URL}" -O - | tar xzvf -
-popd
-mv "/tmp/${ARTIFACT_ID}" public
+wget -O "${ARTIFACT_ID}.tgz" "${DOWNLOAD_URL}"
+tar xvzf "${ARTIFACT_ID}".tgz
+mv "${ARTIFACT_ID}" public
